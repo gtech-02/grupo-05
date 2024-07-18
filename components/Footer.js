21da25dc4b4e0ac4a5e0355a15162de6c9a6df7c
Paulo Luan <pauloluan1409@gmail.com>
2024-07-18 08:46:53 -0300

    Adicionando componentes

---
+++ components/Footer.js
@@ -0,0 +1,57 @@
+export default function Footer(){
+    return `
+    <div class="container">
+        <div class="row">
+          <div class="col coluna">
+            <img src="./images/logo-footer.svg" alt="" />
+          </div>
+          <div class="col">
+            <h2 class="titulo-footer">Informação</h2>
+          </div>
+          <div class="col">
+            <h2 class="titulo-footer">Categorias</h2>
+          </div>
+          <div class="col">
+            <h2 class="titulo-footer">Contato</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col coluna">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore.
+            </p>
+            <div class="icones-footer">
+              <i class="fa-brands fa-facebook-f"></i>
+              <i class="fa-brands fa-instagram"></i>
+              <i class="fa-brands fa-twitter"></i>
+            </div>
+          </div>
+          <div class="col">
+            <p>Sobre Drip Store</p>
+            <p>Segurança</p>
+            <p>Wishilist</p>
+            <p>Blog</p>
+            <p>Trabalhe conosco</p>
+            <p>Meus pedidos</p>
+          </div>
+          <div class="col">
+            <p>Camisetas</p>
+            <p>Calças</p>
+            <p>Bonés</p>
+            <p>Headphones</p>
+            <p>Tênis</p>
+          </div>
+          <div class="col">
+            <p>
+              Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE,
+              60150-161
+            </p>
+            <p>(85) 3051-3411</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <p class="copy-footer">&copy; 2024 Digital College</p>
+    `
+}
---
+++ main.js
@@ -0,0 +1,5 @@
+import Footer from "./components/Footer.js";
+
+let footer = document.getElementById('footer')
+
+footer.innerHTML = Footer();
