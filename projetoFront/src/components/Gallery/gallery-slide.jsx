f87f85e574c8f9e6302f288430587256246b8ca9
samuelpsilva <samuelpsilva54@gmail.com>
2024-07-18 11:59:39 -0300

    adicionando gallery no projeto em react

---
+++ projetoFront/src/components/Gallery/gallery-slide.jsx
@@ -0,0 +1,42 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./gallery-slide.scss";
+
+const Gallery = () => {
+    return (
+<div id="carouselExampleDark" class="carousel carousel-dark slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="/images/home-slide-6.jpeg" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="/images/home-slide-1.jpeg" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="/images/home-slide-7.jpeg" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+  )
+}
+
+export default Gallery
