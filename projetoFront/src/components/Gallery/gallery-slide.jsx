86acda4a25fda23895c2b45bd91a25cea1d10e39
samuelpsilva <samuelpsilva54@gmail.com>
2024-07-19 08:54:46 -0300

    Criando componente gallery

--- projetoFront/src/components/Gallery/gallery-slide.jsx
+++ projetoFront/src/components/Gallery/gallery-slide.jsx
@@ -3,34 +3,30 @@ import "./gallery-slide.scss";
 
 const Gallery = () => {
     return (
-<div id="carouselExampleDark" class="carousel carousel-dark slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+
+      <div id="carouselExampleRide" class="carousel slide" data-bs-ride="true">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src="/images/home-slide-6.jpeg" class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-      </div>
+    <div class="carousel-item active">
+      <img src="images/home-slide-6.jpeg" class="d-block w-100" alt="..."/>
     </div>
-    <div class="carousel-item" data-bs-interval="2000">
-      <img src="/images/home-slide-1.jpeg" class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-      </div>
+    <div class="carousel-item">
+      <img src="images/home-slide-7.jpeg" class="d-block w-100" alt="..."/>
     </div>
-    <div className="carousel-item">
-      <img src="/images/home-slide-7.jpeg" class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block">
-      </div>
+    <div class="carousel-item">
+      <img src="images/home-slide-1.jpeg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="images/home-slide-9.jpg" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="images/home-slide-10.jpg" class="d-block w-100" alt="..."/>
     </div>
   </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
