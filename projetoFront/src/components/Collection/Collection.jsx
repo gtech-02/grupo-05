36ee653f32d0b11c18f090da87df2f1966d39536
samuelpsilva <samuelpsilva54@gmail.com>
2024-07-19 09:51:12 -0300

    adicionando component Collection na home page

---
+++ projetoFront/src/components/Collection/Collection.jsx
@@ -0,0 +1,25 @@
+import "./Collection.scss"
+
+const Collection = () => {
+    return (
+
+        <div class="container text-center" id="colecao">
+            <h2> Coleções em Destaque</h2>
+  <div class="row">
+    <div class="col">
+      <img src="images/collection-1.png"/>
+    </div>
+    <div class="col">
+      <img src="images/collection-2.png"/>
+    </div>
+    <div class="col">
+      <img src="images/collection-3.png"/>
+    </div>
+  </div>
+</div>
+
+
+    )  
+}
+
+export default Collection
