48d58afdc670b9d53720307e1c1ea7bdca352d63
Paulo Luan <pauloluan1409@gmail.com>
2024-07-20 15:42:48 -0300

    Finalizando componente ProductListing com sua propriedades e criando os arquivos json

--- projetoFront/src/components/ProductListing/ProductListing.jsx
+++ projetoFront/src/components/ProductListing/ProductListing.jsx
@@ -1,95 +1,30 @@
 import ProductCard from "../ProductCard/ProductCard"
 import './ProductListing.scss'
-export default function ProductListing({ limit }){
-   let produtos =  [
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 200,
-            priceDiscount: 0
-        },
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 49.9,
-            priceDiscount: 0
-        },
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 49.9,
-            priceDiscount: 0
-        },
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 49.9,
-            priceDiscount: 0
-        },
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 49.9,
-            priceDiscount: 0
-        },
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 49.9,
-            priceDiscount: 0
-        },
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 49.9,
-            priceDiscount: 0
-        },
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 49.9,
-            priceDiscount: 0
-        },
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 49.9,
-            priceDiscount: 0
-        },
-        {
-            name: "K-Swiss V8 - Masculino",
-            image: "https://github.com/digitalcollegebr/projeto-digital-store/blob/main/public/produc-image-1.jpeg?raw=true",
-            price: 49.9,
-            priceDiscount: 0
-        }
-      
-        ];
+export default function ProductListing({legenda, produtos, limit }){
+   
 
-        let produtosLimitados = produtos.slice(0, limit)
+        let produtosLimitados = limit ? produtos.slice(0, limit) : produtos;
 
-        let produtos1 = produtosLimitados.map(produto =>
-            <ProductCard key={produto.name} name={produto.name} image={produto.image} price={produto.price} priceDiscount={produto.priceDiscount}/>);
-        
-        // let produtos1 = []
-
-        // for(let i = 0; i<produtos.length; i++){
-        //     produtos1[i] = <ProductCard name={produtos[i].name} price={produtos[i].price}/>
-        // }
-
-    return (
-        <>
-        <div className="flex">
-        <div className="content">
-        
-
-        <h2 className="titleCards">Produtos em Alta</h2>
-        </div>  
-        <div className="cards">
+        return (
+                <>
+                <div className="flex">
+                <div className="content">
             
-
-                    {produtos1}
-        </div>
-        </div>
-        </>
-    )
-}
+            
+                <h2 className="titleCards">{legenda}</h2>
+                </div>  
+            <div className="cards">
+            
+                {produtosLimitados.map(produto =>(
+                    <ProductCard key={produto.id} product={produto}/>
+                ))}
+            </div>
+    
+                        
+            </div>
+            
+            </>
+        )
+    }
+    
+    
