8207b61bf42c915a7b11ce25c3e066b078db6247
samuelpsilva <samuelpsilva54@gmail.com>
2024-07-20 19:30:53 -0300

    adicionando elemento ordenar na 2a pagina

--- projetoFront/src/pages/ProductListingPage/ProductListingPage.jsx
+++ projetoFront/src/pages/ProductListingPage/ProductListingPage.jsx
@@ -1,11 +1,13 @@
 import Layout from "../Layout/Layout";
 import ProductListing from "../../components/ProductListing/ProductListing";
 import produtosList from '../../json/produtosListing.json'
+import Ordenar from "../../components/OrdenarPor/ordenarpor";
 
 const ProductListingPage = () => {
     return (
         <>
         <Layout>
+            <Ordenar/>
             <ProductListing limit={15}  produtos={produtosList}/>
         </Layout>
         </>
