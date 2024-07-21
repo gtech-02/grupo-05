e1650dbf7f3e53e91de1c37fd4e96e100c8d33db
Paulo Luan <pauloluan1409@gmail.com>
2024-07-21 15:01:25 -0300

    Adicionando componente FilterGroup da página de Produtos

--- projetoFront/src/components/FilterGroup/FilterGroup.jsx
+++ projetoFront/src/components/FilterGroup/FilterGroup.jsx
@@ -1,11 +1,24 @@
+import Options from './Opcoes/Options'
 import './FilterGroup.scss'
-export default function FilterGroup(props){
+import marca from '../../json/marca.json'
+import categoria from '../../json/categoria.json'
+import genero from '../../json/genero.json'
+import estado from '../../json/estado.json'
+export default function FilterGroup(){
     return (
         <>
         <div className="containerFilter">
             <h2 className="subtitle">Filtrar por</h2>
             <span className="linha"></span>
-            
+            <div className='options'>
+            <Options categoria={"Marca"} items={marca}/>
+            <Options categoria={"Categoria"} items={categoria}/>
+            <Options categoria={"Gênero"} items={genero}/>
+            <Options categoria={"Estado"} items={estado}/>
+
+            </div>
+
+
             <div className='content'></div>
         </div>
         </>
