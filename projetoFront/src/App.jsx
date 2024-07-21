87e3805cf01fdaab46f736c2223c94a3423d57c8
miller0803 <millerluis0803@gmail.com>
2024-07-20 21:31:00 -0300

    Iniciando estrutura de página de detalhe do produto

--- projetoFront/src/App.jsx
+++ projetoFront/src/App.jsx
@@ -1,4 +1,5 @@
 import Home from './pages/HomePage/Home';
+import ProductDetails from './pages/ProductDetails/ProductDetails';
 import ProductListing from './pages/ProductListingPage/ProductListingPage'
 import ProductView from './pages/ProductViewPage/ProductView'
 import { BrowserRouter as Router, Route, Routes,  Navigate } from 'react-router-dom';
@@ -13,6 +14,7 @@ function App() {
     <Routes>
         <Route exact path="/home" element={<Home/>} />
         <Route path="/produtos" element={<ProductListing/>} />
+        <Route path="/produtos/:id" element={<ProductDetails />} />
         <Route path="/categorias" element={<ProductView />} />
         <Route path='*' element={<Navigate to='/home' />} />
     </Routes>
--- projetoFront/src/components/ProductCard/ProductCard.jsx
+++ projetoFront/src/components/ProductCard/ProductCard.jsx
@@ -1,32 +1,15 @@
-import './ProductCard.scss'
-export default function ProductCard({product}){
+import { Link } from 'react-router-dom';
+import './ProductCard.scss';
 
-
-    
+export default function ProductCard({product}) {
     return (
-        <>
-
-      <div>
-      <div className="product-card">
-        <img className='img' src={product.image} alt="Produto 1"/>
-        <p className='tipo'>Tênis</p>
-        <h3 className='nomeProduto'>{product.name}</h3>
-        <p className='price'>R$ {product.price}</p>
-        
-      </div>
-    
-       </div> </>
-    )
+        <Link to={`/produtos/${product.id}`} style={{ textDecoration: 'none' }}>
+            <div className="product-card">
+                <img className='img' src={product.image} alt={product.name}/>
+                <p className='tipo'>Tênis</p>
+                <h3 className='nomeProduto'>{product.name}</h3>
+                <p className='price'>R$ {product.price}</p>
+            </div>
+        </Link>
+    );
 }
-    {/* <div classNameName="card">
-        <div className="imagem">
-        <img className="img" src={props.image} alt="" />
-        </div>
-        <div className="infos">
-        <h2>{props.name}</h2>
-        <div className='precos'>
-        <h3 className={props.priceDiscount>0 ? 'priceNone' : 'price'}>{props.price}</h3>
-        <h3 className='priceDiscount'>{props.priceDiscount}</h3>
-        </div>
-        </div>
-    </div> */}
